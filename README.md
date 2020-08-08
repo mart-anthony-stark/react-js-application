6593f9aa50e1aa65b21a8090ce893d33bd5a65e3
MartAnthonyStark404 <martsalazar0@gmail.com>
2020-08-08 23:42:37 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-js-application
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-js-application)
---
+++ index.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+function AddPersonForm(props) {
+  const [ person, setPerson ] = useState('');
+    
+  function handleChange(e) {
+    setPerson(e.target.value);
+  }
+    
+  function handleSubmit(e) {
+    if(person !== '') {
+      props.handleSubmit(person);
+      setPerson('');
+    }
+    e.preventDefault();
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" 
+        placeholder="Add new contact" 
+        onChange={handleChange} 
+        value={person} />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+
+function PeopleList(props) {
+  const arr = props.data;
+  const listItems = arr.map((val, index) =>
+    <li key={index}>{val}</li>
+  );
+  return <ul>{listItems}</ul>;
+}
+
+function ContactManager(props) {
+  const [contacts, setContacts] = useState(props.data);
+
+  function addPerson(name) {
+    setContacts([...contacts, name]);
+  }
+
+  return (
+    <div>
+      <AddPersonForm handleSubmit={addPerson} />
+      <PeopleList data={contacts} />
+    </div>
+  );
+}
+const contacts = ["Mart", "Anthony", "Salazar"];
+
+ReactDOM.render(
+  <ContactManager data={contacts} />, 
+  document.getElementById('root')
+);
